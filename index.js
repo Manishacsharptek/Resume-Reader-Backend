cb0b4c50203d535f6c79ab718a2614a9110d3520
Manisha3435 <manishasinghrajput3435@gmail.com>
2026-03-24 15:26:06 +0530

    feat: Enhance CORS configuration to support multiple dynamic origins with custom validation logic.

--- index.js
+++ index.js
@@ -14,10 +14,33 @@ const app = express();
 // middleware
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:5173",
-  credentials: true
-}));
+// app.use(cors({
+//   origin: process.env.FRONTEND_URL || "http://localhost:5173",
+//   credentials: true
+// }));
+
+const allowedOrigins = [
+  process.env.FRONTEND_URL,
+  "http://localhost:5173"
+];
+console.log(allowedOrigins);
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // allow requests with no origin (like mobile apps / Postman)
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        console.log("Blocked by CORS:", origin);
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 app.use("/api/auth", authRouter);
 app.use("/api/resume", resumeRouter);
 
